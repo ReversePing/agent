393c68e1b53d42559933bd85d2833afe7ff62fa8
Alex Grinman <me@alexgr.in>
2021-09-26 08:35:03 -0400

    add windows ping variant

--- src/discovery/ping.rs
+++ src/discovery/ping.rs
@@ -2,15 +2,12 @@ use futures::future;
 use ipnetwork::IpNetworkError;
 use itertools::Itertools;
 use std::{net::IpAddr, time::Duration};
-use surge_ping::SurgeError;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
     #[error("invalid ip address found")]
     IpError(#[from] IpNetworkError),
-    #[error("failed to ping device")]
-    SurgePing(#[from] SurgeError),
 }
 
 #[derive(Debug)]
@@ -40,22 +37,29 @@ pub async fn ping_subnet(ip: IpAddr, mask: IpAddr) -> Result<Vec<PingResult>, Er
 }
 
 async fn ping_ips(ips: Vec<IpAddr>) -> Vec<PingResult> {
-    let pingers = ips
-        .into_iter()
-        .map(surge_ping::Pinger::new)
-        .filter_map(Result::ok)
-        .map(|mut pinger| {
-            pinger.timeout(Duration::from_secs(2));
-            async move { pinger.ping(0).await }
-        });
+    let pingers = ips.into_iter().map(ping);
 
     future::join_all(pingers)
         .await
         .into_iter()
         .filter_map(Result::ok)
-        .map(|(reply, duration)| PingResult {
-            ip: reply.source,
-            duration,
-        })
+        .map(|(ip, duration)| PingResult { ip, duration })
         .collect()
+}
+
+#[cfg(not(windows))]
+async fn ping(ip: IpAddr) -> Result<(IpAddr, Duration), Box<dyn std::error::Error>> {
+    let mut pinger = surge_ping::Pinger::new(ip)?;
+    pinger.timeout(Duration::from_secs(2));
+    let (_, duration) = pinger.ping(0).await?;
+    Ok((ip, duration))
+}
+
+#[cfg(windows)]
+async fn ping(ip: IpAddr) -> Result<(IpAddr, Duration), Box<dyn std::error::Error>> {
+    let mut pinger = winping::AsyncPinger::new();
+    pinger.set_timeout(2);
+    let buf = winping::Buffer::with_data(vec![0]);
+    let duration = pinger.send(ip, buf).await.result?;
+    Ok((ip, duration))
 }
